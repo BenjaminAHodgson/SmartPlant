8ac847317a1be8158e62c9e4f7d558013c486ccc
bhodgs <41565294+bhodgs@users.noreply.github.com>
2018-08-15 16:49:25 +1000

    Add files via upload

---
+++ project1BenjaminHodgson.ino
@@ -0,0 +1,24 @@
+void setup() {
+ Serial.begin(9600);
+ pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() {
+  int val;
+  val = analogRead(0); //connect sensor to Analog 0
+  Serial.println(val); //print the value to serial port
+  digitalWrite(LED_BUILTIN, LOW);
+  if (val <= 400)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    return 0;
+  }
+  else if (val >= 400)
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+    return 0;
+  }
+}
